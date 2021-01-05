5d7ed7165a9397c7bf97c667cc74de148caaa9a6
anasswalha95 <anas.swalha@yahoo.com>
2021-01-05 16:18:16 +0200

    finishing lap 03 201

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 'use strict'
-let q1, q2, q3, q4, q5, username, d = document, conter = 0, flag = true;
+let q1, q2, q3, q4, q5, q6, q7, username, d = document, conter = 0, flag = true, gussing_number_counter = 5, askUser = true, colors = ["yellow", "green", "red"];
 
 
 function closeMessageDialog() {
+
     username = prompt("What is your name?")
     d.getElementById('box').style.display = "none";
     d.getElementById('welcoming-user').innerHTML = "Welcome," + username;
@@ -16,7 +17,7 @@ function questions() {
 
     q1 = prompt("Do I people who talks too much?")
     q1 = q1.toLocaleLowerCase();
-    if (questionsValdation(q1) == true && (q1 == "yes" || q1 == "y")) {
+    if (questionsValdation(q1) == true && (q1 == "n" || q1 == "no")) {
         alert("Correct ! ");
         //console.log("Question Number 1 correct")
         conter++;
@@ -66,10 +67,76 @@ function questions() {
         alert("False !!")
     }
 
+
+
+    while (askUser) {
+        q6 = prompt("What is my favorite number between 0 & 100 ? attempts: " + gussing_number_counter)
+
+
+
+        switch (true) {
+            case (q6 == 7):
+                alert("correct");
+                conter++;
+                askUser = false
+                break;
+
+            case (q6 == ""):
+                alert("Nothing is entered");
+                break;
+            case (q6 > "20"):
+                alert("too High");
+                break;
+            case (q6 < "0"):
+                alert("too low");
+                break;
+            case (q6 < "20" && q6 > "10"):
+                alert("little Close");
+                break;
+            case (q6 < "10" && q6 > "0"):
+                alert("very Close");
+                break;
+
+
+        }
+
+        --gussing_number_counter;
+
+        if (gussing_number_counter == 0) {
+            alert("You ran out of attempts The correct answer is 7!!");
+            break;
+        }
+
+    }
+    askUser = true;
+    gussing_number_counter = 6;
+
+    while (askUser) {
+        q7 = prompt("What is my favorite color red, purple, green, yellow, black, white  ? attempts: " + gussing_number_counter)
+
+        for (var index = 0; index < colors.length; index++) {
+
+            if (q7 == colors[index]) {
+                alert("correct the answers is yellow, green, and red !!")
+                conter++
+                askUser = false
+                break;
+            }
+
+        }
+
+        --gussing_number_counter;
+
+        if (gussing_number_counter == 0) {
+            alert("You ran out of attempts The correct answers is yellow, green, and red!!");
+            break;
+        }
+    }
+
     d.getElementById('box').style.display = "block";
     d.getElementById('close').style.display = "block";
-
-    d.getElementById('box-text').innerHTML = "Welcome " + username + " you got " + conter + " out of 5";
+    alert(" you got " + conter + " out of 7");
+    d.getElementById('box-text').innerHTML = "Welcome " + username + " you got " + conter + " out of 7";
 
 
 
